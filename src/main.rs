b6d485e299c08a8a214b97201989736df1a46820
James Gill <jagill@fb.com>
2023-03-25 16:08:55 -0400

    Initial repl scaffolding

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use anyhow::{bail, Result as AnyResult};
+use std::io::Write;
+// use log::{debug, info, warn};
+
+fn main() -> AnyResult<()> {
+    let mut args = std::env::args();
+    let _prog_name = args.next().expect("Expected program name in args");
+    let script_path_opt = args.next();
+    match args.next() {
+        None => (),
+        Some(_) => bail!("Usage: lox [script]"),
+    }
+    if let Some(script_path) = script_path_opt {
+        run_file(&script_path)
+    } else {
+        run_prompt()
+    }
+}
+
+fn run_file(script_path: &str) -> AnyResult<()> {
+    let contents = std::fs::read_to_string(script_path)?;
+    run(&contents)
+}
+
+fn run_prompt() -> AnyResult<()> {
+    do_prompt()?;
+    let lines = std::io::stdin().lines();
+    for line_res in lines {
+        let line = line_res?;
+        run(&line)?;
+        do_prompt()?;
+    }
+    Ok(())
+}
+
+fn do_prompt() -> AnyResult<()> {
+    print!("> ");
+    std::io::stdout().flush()?;
+    Ok(())
+}
+
+fn run(contents: &str) -> AnyResult<()> {
+    println!("Got > {}", contents);
+    Ok(())
+}
